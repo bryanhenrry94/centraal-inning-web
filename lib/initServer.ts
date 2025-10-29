a6436545bb2c11b7be1f61feff2f4f95cba12ee4
Bryan Navarrete <bryanhenrry94@gmail.com>
2025-10-29 02:22:42 -0500

    especifica puerto para render

--- lib/initServer.ts
+++ lib/initServer.ts
@@ -1,6 +1,10 @@
 // Solo ejecutamos el scheduler en el servidor
 if (typeof window === "undefined") {
-  import("./scheduler").then(({ startJobScheduler }) => {
-    startJobScheduler();
-  });
+  if (process.env.NODE_ENV === "production") {
+    import("./scheduler").then(({ startJobScheduler }) => {
+      startJobScheduler();
+    });
+  } else {
+    console.log("⚠️ Scheduler disabled in production");
+  }
 }
